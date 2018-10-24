f78472a842b0851c2c8c7dc03527eba16a540ccc
Lewis Youl <lewisyoul@gmail.com>
2018-10-24 08:30:03 +0100

    [feature] add posts controller and index route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
   resources :projects, only: [:index]
+  resources :posts, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  describe 'GET index' do
+    it 'returns 200 OK' do
+      get :index
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the index template' do
+      expect(get :index).to render_template(:index)
+    end
+  end
+end
