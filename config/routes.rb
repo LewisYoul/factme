a06360ce4f2998ce898ff4b127b878a89c51e4e6
Lewis Youl <lewisyoul@gmail.com>
2018-10-24 08:23:36 +0100

    [feature] create projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :about
+  resources :about, only: [:index]
+  resources :projects, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AboutController, type: :controller do
-    fdescribe 'GET index' do
-        it 'returns 200 OK' do
-          get :index
-    
-          expect(response).to have_http_status(:ok)
-        end
-    
-        it 'renders the about page template' do
-          get :index
-    
-          expect(get :index).to render_template(:index)
-        end
+  describe 'GET index' do
+      it 'returns 200 OK' do
+        get :index
+  
+        expect(response).to have_http_status(:ok)
       end
+  
+      it 'renders the about page template' do
+        expect(get :index).to render_template(:index)
+      end
+    end
 end
