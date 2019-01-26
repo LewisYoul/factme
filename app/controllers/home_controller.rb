01da3deeb357661555b7905004933ea19866872c
Lewis Youl <lewisyoul@gmail.com>
2019-01-26 17:39:37 +0000

    [styling] Style post creation form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @recent_posts = Post.order(created_at: :desc).last(5)
+    @recent_posts = Post.order(created_at: :desc).first(5)
   end
 
 end
