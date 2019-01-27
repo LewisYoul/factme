27787aa70314efa2c73588154da24b1ff45e496c
Lewis Youl <lewisyoul@gmail.com>
2019-01-27 20:01:53 +0000

    [feature] Create habl2m relationship between tags + posts

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,26 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    tag = Tag.create(tag_params)
+
+    if tag.save
+      redirect_to(:tags)
+    else
+      redirect_to(:new_tag)
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :about, only: [:index]
   resources :projects, only: [:index]
   resources :posts, only: [:index, :new, :create, :show]
+  resources :tags, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
