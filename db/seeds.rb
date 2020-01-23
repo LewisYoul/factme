7e79408ef03715744668ea0eea82b1e3c3a03373
LewisYoul <lewisyoul@gmail.com>
2020-01-23 08:27:51 +0000

    [config] Add post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+i = 1
+
+Post.destroy_all
+
+30.times do
+  Post.create!(
+    title: "Stock post #{i}",
+    body_one: "Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar Lorem impsum dolar ",
+    synopsis: "A short synopsis about the post",
+    published: true
+  )
+
+  i += 1
+end
